77a46843ed3e1359761624a7ecf2f6eaf515e6af
Tabish Ali Ansari <ansaritabishali1@gmail.com>
2025-12-29 02:16:45 +0530

    first commit

---
+++ addition.py
@@ -0,0 +1,6 @@
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
+sum_result = num_1 + num_2
+
+print("The sum of", num_1, "and", num_2, "is:", sum_result)
